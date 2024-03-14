a3a2fd8c7c57a57e13730e5ceca6ce88b55217be
Felipe Saboya <felipe.saboya@outlook.pt>
2024-03-14 15:23:03 +0000

    feat: desafio-3-clean-architecture

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
+# Readme
+
+## Passos
+
+### 1 DockerCompose
+
+```
+docker-compose up -d
+```
+
+### 2 MySQL
+
+```
+docker-compose exec mysql bash
+mysql -uroot -p orders
+CREATE TABLE orders (id varchar(255) NOT NULL, price float NOT NULL, tax float NOT NULL, final_price float NOT NULL, PRIMARY KEY (id));
+```
+
+### 3 Start
+
+```
+go run cmd/ordersystem/main.go cmd/ordersystem/wire_gen.go
+```
+
+> Play :)
+ 
+## Portas
+
+### Web HTTP
+
+localhost:8000
+
+### gRPC
+
+localhost:50051
+
+### GraphQL
+
+localhost:8080
+
+---
+
 # desafio-3-clean-architecture
+
+Olá devs!
+Agora é a hora de botar a mão na massa. Para este desafio, você precisará criar o usecase de listagem das orders.
+Esta listagem precisa ser feita com:
+- Endpoint REST (GET /order)
+- Service ListOrders com GRPC
+- Query ListOrders GraphQL
+  Não esqueça de criar as migrações necessárias e o arquivo api.http com a request para criar e listar as orders.
+
+Para a criação do banco de dados, utilize o Docker (Dockerfile / docker-compose.yaml), com isso ao rodar o comando docker compose up tudo deverá subir, preparando o banco de dados.
+Inclua um README.md com os passos a serem executados no desafio e a porta em que a aplicação deverá responder em cada serviço.
---
+++ internal/infra/web/order_handler.go
@@ -0,0 +1,48 @@
+package web
+
+import (
+	"encoding/json"
+	"github.com/felipe-saboya/desafio-3-clean-architecture/internal/entity"
+	"github.com/felipe-saboya/desafio-3-clean-architecture/internal/usecase"
+	"github.com/felipe-saboya/desafio-3-clean-architecture/pkg/events"
+	"net/http"
+)
+
+type WebOrderHandler struct {
+	EventDispatcher   events.EventDispatcherInterface
+	OrderRepository   entity.OrderRepositoryInterface
+	OrderCreatedEvent events.EventInterface
+}
+
+func NewWebOrderHandler(
+	EventDispatcher events.EventDispatcherInterface,
+	OrderRepository entity.OrderRepositoryInterface,
+	OrderCreatedEvent events.EventInterface,
+) *WebOrderHandler {
+	return &WebOrderHandler{
+		EventDispatcher:   EventDispatcher,
+		OrderRepository:   OrderRepository,
+		OrderCreatedEvent: OrderCreatedEvent,
+	}
+}
+
+func (h *WebOrderHandler) Create(w http.ResponseWriter, r *http.Request) {
+	var dto usecase.OrderInputDTO
+	err := json.NewDecoder(r.Body).Decode(&dto)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	createOrder := usecase.NewCreateOrderUseCase(h.OrderRepository, h.OrderCreatedEvent, h.EventDispatcher)
+	output, err := createOrder.Execute(dto)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = json.NewEncoder(w).Encode(output)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
